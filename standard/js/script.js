da303c266f1a6a7cafd76c7412630a5cd5f7b7d5
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-20 17:34:15 +0100

    feat: add milestone 2

--- standard/js/script.js
+++ standard/js/script.js
@@ -37,3 +37,22 @@ console.log(teamMembers)
 for (let i = 0; i < teamMembers.length; i++){
     console.log(`Ecco un membro del team: ${teamMembers[i]["name"]}, il cui ruolo è ${teamMembers[i]["role"]} e il file della sua immagine è ${teamMembers[i]["photo"]}`);
 }
+
+//stamparle nel dom, verisone più estesa tanto per scomporre i passaggi e averli visivamente più chiari 
+const listNames = document.querySelector("#name-list")
+const listRole = document.querySelector("#role-list")
+const listPhotos = document.querySelector("#photo-list")
+
+for (let i = 0; i < teamMembers.length; i++){
+    let newDivForName = document.createElement("div");
+    let newDivForRole = document.createElement("div");
+    let newDivForPhoto = document.createElement("div");
+
+    newDivForName.innerHTML = teamMembers[i]["name"];
+    newDivForRole.innerHTML = teamMembers[i]["role"];
+    newDivForPhoto.innerHTML = teamMembers[i]["photo"];
+
+    listNames.append(newDivForName);
+    listRole.append(newDivForRole);
+    listPhotos.append(newDivForPhoto);
+}
